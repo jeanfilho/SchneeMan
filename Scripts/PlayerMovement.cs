8ddb313ee1dd192906ab7b1647210553f790f620
jeanfilho <jeanfilho@gmail.com>
2015-08-10 15:01:37 +0200

    Added simple player movement

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//Components of Game Object
+	PlayerStatus status;
+	CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = gameObject.GetComponent<CharacterController>();
+		status = gameObject.GetComponent<PlayerStatus>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w"))
+			controller.SimpleMove(transform.forward * status.speed);
+	}
+}
--- Scripts/PlayerStatus.cs
+++ Scripts/PlayerStatus.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
+public class PlayerStatus : MonoBehaviour {
+
+	//Health, status and abilities
+	public int hp = 100;
+	public int speed = 10;
 
 	// Use this for initialization
 	void Start () {
