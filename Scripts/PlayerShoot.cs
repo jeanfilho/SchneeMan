97f6f5f1da193149e42cafd63fd8a88a819a0892
bastianheinzelmann <bastian.heinzelmann@hotmail.de>
2015-08-10 16:05:16 +0100

    PlayerShoot implementiert

---
+++ Scripts/PlayerShoot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	public Transform player;
+	public float thrust_x;  //Wurfstärke des Spielers
+	public float thrust_y;
+	public float thrust_z;
+	//public GameObject Ball;
+
+	private float mousePosition_x;
+	private float mousePosition_y;
+	private float mousePosition_z;
+	private Vector3 shootDirection;
+
+
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+
+			rb.useGravity = true;
+			rb.AddForce (thrust_x, thrust_y, 0f);
+		}
+
+	}
+}
