9dcdaa1907c6158a6e89db24a33e661b87422577
jeanfilho <jeanfilho@gmail.com>
2015-08-10 18:19:45 +0200

    Added Trees and water

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	//Physic Simulation
 	RaycastHit hitInfo;
+	bool isGrounded = false;
 
 	// Use this for initialization
 	void Start () {
@@ -31,14 +32,22 @@ public class PlayerMovement : MonoBehaviour {
 
 		//Movement drag
 		rbody.AddForce(new Vector3(-rbody.velocity.x * horizontalDrag, -3, -rbody.velocity.z * horizontalDrag));
-		
 
 		//Jump function
-		if (Input.GetButtonDown("Jump") &&
-		    Physics.Raycast (new Ray (gameObject.transform.position, new Vector3(0,-1,0)), out hitInfo, 0.6f)) {
+		if(!isGrounded &&
+		   Physics.Raycast (new Ray (gameObject.transform.position, new Vector3(0,-1,0)), out hitInfo, 0.65f)){
+			isGrounded = true;
+		}
+		if (Input.GetButtonDown("Jump") && isGrounded){
 			rbody.AddForce (gameObject.transform.up * status.jumpforce);
+			isGrounded = false;
 		}
 		Debug.Log (rbody.velocity.magnitude);
 
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.name.Equals ("Water"))
+			status.hp = 0;
 	}
 }
