8fefd91d669af90d670bb52cb856c617ea6de9b8
jeanfilho <jeanfilho@gmail.com>
2015-08-10 17:01:06 +0200

    PlayerStatus changed

--- Scripts/PlayerStatus.cs
+++ Scripts/PlayerStatus.cs
@@ -5,13 +5,13 @@ public class PlayerStatus : MonoBehaviour {
 
 	//Health, status and abilities
 	public float hp = 100;
-	public float speed = 10;
+	public float speed = 30;
 	public float turnspeed = 10;
-	public float jumpforce = 10;
+	public float jumpforce = 300;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
