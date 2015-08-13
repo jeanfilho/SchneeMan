59a00944d309bef60f6ef618019499836c71ae18
bastianheinzelmann <bastian.heinzelmann@hotmail.de>
2015-08-13 12:27:39 +0100

    EnemyCreator with online Feature

---
+++ Bastian/EnemyCreator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCreator : Photon.MonoBehaviour {
+
+	public Transform[] spawnPoints;	//Array von Emptys (SpawnPoints)
+	public int totalEnemies;	//Anzahl der Krabbengegner die gespawnt werden
+	public GameObject[] enemy;	//Array von Gegnerarten
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (GameManager.isMultiplayer) {
+
+			if (totalEnemies <= spawnPoints.Length) {
+				for (int i = 0; i < totalEnemies; i++) {
+					PhotonNetwork.Instantiate (enemy [Random.Range (0, enemy.Length)].name, spawnPoints [i].position, Quaternion.identity,0);
+				}
+			}
+		} 
+		else 
+		{
+			for (int i = 0; i < totalEnemies; i++) {
+				Instantiate (enemy [Random.Range (0, enemy.Length)], spawnPoints [i].position, Quaternion.identity);
+			}
+		}
+	}
+}
